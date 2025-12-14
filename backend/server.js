048765a3801f7249df007121995fa6337eac71b7
Chris-wav <HristosKalatzis@outlook.com.gr>
2025-12-14 17:39:09 +0200

    filters

--- backend/server.js
+++ backend/server.js
@@ -5,26 +5,22 @@ import axios from "axios";
 const app = express();
 app.use(cors());
 
-// cache για τα δεδομένα των πτήσεων
 let cache = null;
 let lastFetchTime = 0;
-const CACHE_DURATION = 20000; // 20 δευτερόλεπτα
+const CACHE_DURATION = 20000;
 
 app.get("/api/flights", async (req, res) => {
   const now = Date.now();
 
-  // 1. Αν έχουμε cache και ΔΕΝ έχει λήξει → γύρνα την
   if (cache && now - lastFetchTime < CACHE_DURATION) {
     return res.json(cache);
   }
 
-  // 2. Αλλιώς φέρε νέα δεδομένα από OpenSky
   try {
     const response = await axios.get(
-      "https://opensky-network.org/api/states/all?lamin=34&lomin=19&lamax=42&lomax=29"
+      "https://opensky-network.org/api/states/all"
     );
 
-    // 3. Αποθήκευση στο cache
     cache = response.data;
     lastFetchTime = now;
 
@@ -32,7 +28,6 @@ app.get("/api/flights", async (req, res) => {
   } catch (error) {
     console.error("OpenSky API Error:", error.message);
 
-    // 4. Αν έχουμε παλιά cache, δώστην αντί για error
     if (cache) {
       return res.json(cache);
     }
